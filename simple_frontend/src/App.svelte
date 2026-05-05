b34fe1d656a0e6d4e8bebb4add4753cf456186a1
drakstik <dmberingabo@outlook.com>
2026-05-05 10:37:01 +0200

    init commit

---
+++ simple_frontend/src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Counter from "./lib/Counter.svelte";
+  import SayHello from "./lib/Hello_backend.svelte";
+</script>
+
+<section id="center">
+  <Counter />
+  <SayHello />
+</section>
